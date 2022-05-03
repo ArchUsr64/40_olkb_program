2fbba120398deee8aa4fc104b652b03d0762d868
ArchUsr64 <anshulusr@gmail.com>
2022-05-04 02:06:39 +0530

    Added keymap config

---
+++ src/keymap.h
@@ -0,0 +1,9 @@
+#define KEYMAP_ARRAY                                                           \
+  {                                                                            \
+    {TAB, Q, W, E, R, T, Y, U, I, O, P, QUOTATION_MARK},                       \
+        {CTRL_L, A, S, D, F, G, H, J, K, L, SEMI_COLON, CTRL_R},               \
+        {SHIFT_L, Z, X, C, V, B, N, M, COMMA, PERIOD, BACKSPACE, SHIFT_R}, {   \
+      MOUSE_LEFT, MOUSE_DOWN, MOUSE_UP, MOUSE_RIGHT, BACKSPACE, ENTER, SPACE,  \
+          BACKSPACE, MOUSE_1, MOUSE_2, ALT_L, ESC                              \
+    }                                                                          \
+  }
