8342bde2ab5a16585b0232ab2633a3fc63becd35
ArchUsr64 <anshulusr@gmail.com>
2022-05-04 02:24:10 +0530

    Made pin arrays global

--- src/main.h
+++ src/main.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "key_config.h"
+#include "keymap.h"
 
-void setup() {
-  const int row_count = MATRIX_ROWS;
-  const int col_count = MATRIX_ROWS;
+const int row_count = MATRIX_ROWS;
+const int col_count = MATRIX_ROWS;
 
-  int row_pins[MATRIX_ROWS] = MATRIX_ROWS_PINS;
-  int col_pins[MATRIX_COLS] = MATRIX_ROWS_PINS;
-}
+int row_pins[MATRIX_ROWS] = MATRIX_ROWS_PINS;
+int col_pins[MATRIX_COLS] = MATRIX_ROWS_PINS;
+
+void setup() {}
 
 void loop() {}
